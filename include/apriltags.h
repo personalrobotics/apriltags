e5441899d63c7f3e8cdcbe1c8c2b4d6d5a0fe060
awalsman <awalsman@fd1e622a-c74e-0410-99c6-c6d0d21c99a6>
2014-05-19 19:41:55 +0000

    Added apriltags headers

---
+++ include/apriltags.h
@@ -0,0 +1,53 @@
+#define SMALL_TAG_SIZE 0.0358968
+#define MED_TAG_SIZE 0.0630174
+#define PAGE_TAG_SIZE 0.165
+
+#define DEFAULT_TAG_FAMILY string("Tag36h11")
+using namespace std;
+// ROS parts
+ros::NodeHandlePtr node_;
+boost::shared_ptr<image_transport::ImageTransport> image_;
+
+ros::Publisher marker_publisher_;
+
+image_transport::Subscriber image_subscriber;
+ros::Subscriber info_subscriber;
+
+ros::ServiceServer start_service_;
+ros::ServiceServer stop_service_;
+ros::ServiceServer stop_all_service_;
+ros::ServiceServer is_running_service_;
+ros::ServiceServer is_id_open_service_;
+ros::ServiceServer running_ids_service_;
+
+sensor_msgs::CameraInfo camera_info_;
+
+// AprilTag parts
+TagFamily* family_;
+TagDetector* detector_;
+string tag_family_name_;
+TagDetectorParams tag_params;
+string camera_topic_name = "/Image";
+string output_marker_list_topic_name = "marker_array";
+string tag_data;
+
+// Settings and local information
+int viewer_;
+boost::unordered_map<size_t, double> tag_sizes_;
+double default_tag_size_;
+string frame_;
+bool running_;
+
+Eigen::Matrix4d GetDetectionTransform(TagDetection detection);
+void InfoCallback(const sensor_msgs::CameraInfoConstPtr& camera_info);
+void InfoCallback(const sensor_msgs::CameraInfoConstPtr& camera_info);
+void ConnectCallback(const ros::SingleSubscriberPublisher& info);
+void DisconnectCallback(const ros::SingleSubscriberPublisher& info);
+void GetParameterValues();
+void SetupPublisher();
+void InitializeTags();
+void InitializeServices();
+bool StopAllService(apriltags::StopAll::Request &req,apriltags::StopAll::Response &res);
+bool IsRunningService(apriltags::IsRunning::Request &req,apriltags::IsRunning::Response &res);
+void StoreTagData(string tag_data);
+void InitializeROSNode();
--- src/apriltags/apriltags.py
+++ src/apriltags/apriltags.py
@@ -1,24 +1,101 @@
-PACKAGE = 'apriltags_swat'
+PACKAGE = 'apriltags'
 import roslib; roslib.load_manifest(PACKAGE)
 import roslaunch
+import rospy
+import srv
+import atexit
+import percy.detector as detector
 
-visible_tags = None
+'''
+open_ids = set()
 
-class AprilTags(object):
-    def __init__(self, namespace='/'):
-        self.node = roslaunch.core.Node(
-                PACKAGE, 'apriltags', namespace=namespace)
-        self.launch = roslaunch.scriptapi.ROSLaunch()
-        self.launch.start()
-        self.process = None
+def cleanup_apriltags():
+    for open_id in open_ids:
+        print 'Cleaning up', open_id
+        proxy = rospy.ServiceProxy('apriltags/stop', srv.Stop)
+        proxy(open_id)
+
+atexit.register(cleanup_apriltags)
+'''
+
+class AprilTags(detector.Detector):
+    def __init__(self,
+                 namespace='/apriltags',
+                 subscribe_topic='/Image',
+                 publish_topic='marker_array'):
+        self.start_id = 0
+        detector.Detector.__init__(self, namespace, subscribe_topic,
+                publish_topic)
     
+    def SetFamily(family_name):
+        pass
+    
+    '''
     def Start(self):
-        if not self.IsRunning():
-            self.process = launch.launch(node)
+        is_open_proxy = rospy.ServiceProxy('apriltags/is_id_open', srv.IsIdOpen)
+        already_open = is_open_proxy(self.start_id).open
+        if not already_open:
+            try:
+                start_proxy = rospy.ServiceProxy('apriltags/start', srv.Start)
+                self.start_id = start_proxy().id
+                open_ids.add(self.start_id)
+            except rospy.ServiceException:
+                print "Service is not running!"
+                raise
+        
+        return self.start_id
     
     def Stop(self):
-        if self.IsRunning():
-            self.process.stop()
+        try:
+            print 'a'
+            #stop_proxy = rospy.ServiceProxy('apriltags/stop', srv.Stop)
+            print self.start_id
+            print 'b'
+            closed = self.stop_proxy(self.start_id)
+            print 'c'
+            self.start_id = 0
+            print 'd'
+            open_ids.remove(self.start_id)
+            return closed
+        
+        except rospy.ServiceException:
+            print "Service is not running!"
+            raise
+    
+    def StopAll(self):
+        try:
+            stop_proxy = rospy.ServiceProxy('apriltags/stop_all', srv.StopAll)
+            stop_proxy()
+        except rospy.ServiceException:
+            print "Service is not running!"
+            raise
     
     def IsRunning(self):
-        return (self.process is not None) and self.process.is_alive()
+        try:
+            running_proxy = rospy.ServiceProxy('apriltags/is_running',
+                    srv.IsRunning)
+            return running_proxy().running
+        except rospy.ServiceException:
+            print "Service is not running!"
+            raise
+    
+    def AmIOpen(self):
+        try:
+            open_proxy = rospy.ServiceProxy('apriltags/is_id_open',
+                    srv.IsIdOpen)
+            return open_proxy(self.start_id).open
+        except rospy.ServiceException:
+            print "Service is not running!"
+            raise
+    
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, ext_type, exc_value, traceback):
+        print "EXIT!"
+        self.Stop()
+    
+    #def __del__(self):
+    #    print "__del__"
+    #    self.Stop()
+    '''
