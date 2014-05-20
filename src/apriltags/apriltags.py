272685de543376892b68e515b08ed4e4d35c86e4
awalsman <awalsman@fd1e622a-c74e-0410-99c6-c6d0d21c99a6>
2014-05-20 20:40:25 +0000

    removing old comments

--- src/apriltags/apriltags.py
+++ src/apriltags/apriltags.py
@@ -6,18 +6,6 @@ import srv
 import atexit
 import percy.detector as detector
 
-'''
-open_ids = set()
-
-def cleanup_apriltags():
-    for open_id in open_ids:
-        print 'Cleaning up', open_id
-        proxy = rospy.ServiceProxy('apriltags/stop', srv.Stop)
-        proxy(open_id)
-
-atexit.register(cleanup_apriltags)
-'''
-
 class AprilTags(detector.Detector):
     def __init__(self,
                  namespace='/apriltags',
@@ -29,73 +17,3 @@ class AprilTags(detector.Detector):
     
     def SetFamily(family_name):
         pass
-    
-    '''
-    def Start(self):
-        is_open_proxy = rospy.ServiceProxy('apriltags/is_id_open', srv.IsIdOpen)
-        already_open = is_open_proxy(self.start_id).open
-        if not already_open:
-            try:
-                start_proxy = rospy.ServiceProxy('apriltags/start', srv.Start)
-                self.start_id = start_proxy().id
-                open_ids.add(self.start_id)
-            except rospy.ServiceException:
-                print "Service is not running!"
-                raise
-        
-        return self.start_id
-    
-    def Stop(self):
-        try:
-            print 'a'
-            #stop_proxy = rospy.ServiceProxy('apriltags/stop', srv.Stop)
-            print self.start_id
-            print 'b'
-            closed = self.stop_proxy(self.start_id)
-            print 'c'
-            self.start_id = 0
-            print 'd'
-            open_ids.remove(self.start_id)
-            return closed
-        
-        except rospy.ServiceException:
-            print "Service is not running!"
-            raise
-    
-    def StopAll(self):
-        try:
-            stop_proxy = rospy.ServiceProxy('apriltags/stop_all', srv.StopAll)
-            stop_proxy()
-        except rospy.ServiceException:
-            print "Service is not running!"
-            raise
-    
-    def IsRunning(self):
-        try:
-            running_proxy = rospy.ServiceProxy('apriltags/is_running',
-                    srv.IsRunning)
-            return running_proxy().running
-        except rospy.ServiceException:
-            print "Service is not running!"
-            raise
-    
-    def AmIOpen(self):
-        try:
-            open_proxy = rospy.ServiceProxy('apriltags/is_id_open',
-                    srv.IsIdOpen)
-            return open_proxy(self.start_id).open
-        except rospy.ServiceException:
-            print "Service is not running!"
-            raise
-    
-    def __enter__(self):
-        return self
-    
-    def __exit__(self, ext_type, exc_value, traceback):
-        print "EXIT!"
-        self.Stop()
-    
-    #def __del__(self):
-    #    print "__del__"
-    #    self.Stop()
-    '''
