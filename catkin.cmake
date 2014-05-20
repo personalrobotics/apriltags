2bb3a41caaa2a927936e1289488e7e86d5f9d214
pkv <pkv@fd1e622a-c74e-0410-99c6-c6d0d21c99a6>
2014-05-20 23:57:01 +0000

    Added most of the fixes to make this work.

--- catkin.cmake
+++ catkin.cmake
@@ -1,15 +1,17 @@
 cmake_minimum_required(VERSION 2.8.3)
 
+# Set up the ROS Catkin package settings.
 find_package(catkin REQUIRED)
-catkin_package(
-    INCLUDE_DIRS include/
-    LIBRARIES ${PROJECT_NAME}
-    DEPENDS OpenCV Eigen yaml-cpp
-)
+find_package(OpenCV REQUIRED)
+find_package(Eigen REQUIRED)
 
+# Set up the ROS Catkin package settings.
+catkin_package()
+
+# CGAL requires that -frounding-math be set.
 add_definitions(-frounding-math)
 
-# Download the external Swatbotics repository
+# Download the external Swatbotics repository.
 include(ExternalProject)
 ExternalProject_Add(apriltags_swatbotics_EXTERNAL
     PREFIX ${PROJECT_SOURCE_DIR}/build/apriltags
@@ -18,26 +20,30 @@ ExternalProject_Add(apriltags_swatbotics_EXTERNAL
     CMAKE_ARGS -DCMAKE_CXX_FLAGS=-frounding-math -DBUILD_SHARED_LIBS:BOOL=ON
 )
 
-# Recover project paths for additional settings
+# Recover project paths for additional settings.
 ExternalProject_Get_Property(apriltags_swatbotics_EXTERNAL
-  SOURCE_DIR BINARY_DIR INSTALL_DIR )
+  SOURCE_DIR BINARY_DIR INSTALL_DIR)
+set(apriltags_swatbotics_INCLUDE_DIRS
+  "${SOURCE_DIR}/include"
+  CACHE INTERNAL "" FORCE
+)
 
-# Tell cmake that the external project generated a library so we
-# can add dependencies here instead of later
+# Tell CMake that the external project generated a library so we
+# can add dependencies here instead of later.
 add_library(apriltags_swatbotics UNKNOWN IMPORTED)
 set_property(TARGET apriltags_swatbotics
   PROPERTY IMPORTED_LOCATION
-  ${BINARY_DIR}/libapriltags.so
-  )
+  ${BINARY_DIR}/libapriltags.so)
 add_dependencies(apriltags_swatbotics apriltags_swatbotics_EXTERNAL)
-include_directories(${SOURCE_DIR})
+
 
 include_directories(
     include/${PROJECT_NAME}
-    ${Catkin_INCLUDE_DIRS})
+    ${catkin_INCLUDE_DIRS}
+    ${apriltags_swatbotics_INCLUDE_DIRS})
 
 add_executable(apriltags src/apriltags.cpp)
-target_link_libraries(apriltags ${Catkin_LIBRARIES})
+target_link_libraries(apriltags ${catkin_LIBRARIES})
 target_link_libraries(apriltags apriltags_swatbotics)
 
 install(TARGETS apriltags
